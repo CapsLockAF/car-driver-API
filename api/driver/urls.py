9794a0635ff079300a3d96f813a40b7e10bc0cdd
o.fareniuk <fareniuk.olexandr@gmail.com>
2021-12-10 18:19:05 +0200

    feat[driver] new api_view for a list of drivers, filter vehicles by date

--- api/driver/urls.py
+++ api/driver/urls.py
@@ -4,9 +4,10 @@ from .views import *
 app_name = "driver"
 
 urlpatterns = [
-    path('driver/', DriverCreateView.as_view({
-        'get': 'list',
-        'post': 'create'
-    })),
+    # path('driver/', DriverCreateView.as_view({
+    #     'get': 'list',
+    #     'post': 'create'
+    # })),
+    path('driver/', drivers_list),
     path('driver/<int:pk>', DriverDetailView.as_view()),
 ]
--- api/driver/views.py
+++ api/driver/views.py
@@ -1,14 +1,53 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .serializers import DriverDetailSerializer
 from .models import Driver
+from datetime import datetime
 
 
-class DriverCreateView(viewsets.ModelViewSet):
-    serializer_class = DriverDetailSerializer
-    queryset = Driver.objects.all()
+@api_view(['GET', 'POST'])
+def drivers_list(request):
+    """
+    List of drivers, a filter of car drivers by created date,
+     or create a new driver.
+     POST {
+            'first_name': 'str',
+            'last_name': 'str'
+            }
+    filter by date GET /?created_at__gte=10-11-2021 or
+                    /?created_at__gte=10-11-2021
+    """
+
+    if request.method == 'GET':
+        drivers = Driver.objects.all()
+
+        if request.GET and request.GET.get('created_at__gte'):
+            drivers = Driver.objects.filter(
+                created_at__gte=datetime.strptime(
+                    request.GET['created_at__gte'],
+                    '%d-%m-%Y'))
+
+        elif request.GET and request.GET.get('created_at__lte'):
+            drivers = Driver.objects.filter(
+                created_at__lte=datetime.strptime(
+                    request.GET['created_at__lte'],
+                    '%d-%m-%Y'))
+
+        serializer = DriverDetailSerializer(drivers, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = DriverDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DriverDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """Provides get, put, patch and delete method handlers"""
     serializer_class = DriverDetailSerializer
     queryset = Driver.objects.all()
 
